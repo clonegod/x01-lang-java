fac75ab580e14e6d276c66e4ae13c3a51e83304c
clonegod <asynclife@163.com>
2018-05-03 03:04:32 +0800

    update

---
+++ common-utils/clonegod-commons/src/test/java/clonegod/commons/memory/MemoryTest.java
@@ -0,0 +1,13 @@
+package clonegod.commons.memory;
+
+public class MemoryTest {
+	
+	// -Xms30m -Xmx30m -XshowSettings:vm
+	public static void main(String args[]) {
+		System.out.println("Used Memory   :  "
+				+ (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) + " bytes");
+		System.out.println("Free Memory   : " + Runtime.getRuntime().freeMemory() + " bytes");
+		System.out.println("Total Memory  : " + Runtime.getRuntime().totalMemory() + " bytes");
+		System.out.println("Max Memory    : " + Runtime.getRuntime().maxMemory() + " bytes");
+	}
+}
